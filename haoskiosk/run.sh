d45e32a42aff9b57ea39260e2ff2856cc05fc860
GuntherSchulz <155998655+GuntherSchulz01@users.noreply.github.com>
2025-08-22 19:21:45 +0200

    Update run.sh

--- haoskiosk/run.sh
+++ haoskiosk/run.sh
@@ -344,7 +344,8 @@ setxkbmap -query  | sed 's/^/  /' #Log layout
 if [ "$USE_VIRTUAL_KEYBOARD" = true ]; then
     # echo "$(wvkbd-mobintl -L 200 -fg ffffff -fg-sp ffffff --text 000000 --text-sp 000000 -fn 25 &)"
     # svkbd-mobile-intl -d &
-    svkbd-mobile-intl -n -o | cowsay
+    # svkbd-mobile-intl -n -o | cowsay
+    svkbd-mobile-intl -d
     bashio::log.info "Starting svkbd-mobint-intl keyboard"
 fi
 
