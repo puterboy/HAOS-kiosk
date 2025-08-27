7e949e037aa7dbd53c36dd57eee8ae1ae8769462
GuntherSchulz <155998655+GuntherSchulz01@users.noreply.github.com>
2025-08-27 15:46:15 +0200

    Update run.sh debug

--- haoskiosk/run.sh
+++ haoskiosk/run.sh
@@ -144,20 +144,26 @@ export DBUS_SESSION_BUS_ADDRESS
 #### Launch virtual keyboard if needed
 if [ "$USE_VIRTUAL_KEYBOARD" = true ]; then
  	dbus-run-session -- dconf write /org/onboard/auto-show true # enable auto show
+  	echo "$VIRTUAL_KEYBOARD_LAYOUT"
   	if [ -n "$VIRTUAL_KEYBOARD_LAYOUT" ]; then
   		KBD_LAYOUT_FILE='/usr/share/onboard/layouts/'"$VIRTUAL_KEYBOARD_LAYOUT"\'
+		echo "$KBD_LAYOUT_FILE" 
 		if [[ -f "$KBD_LAYOUT_FILE" ]]; then
 			dbus-run-session -- dconf write /org/onboard/layout "$KBD_LAYOUT_FILE" # set default layout
 		fi
 	fi
+    echo "$VIRTUAL_KEYBOARD_THEME"
   	if [ -n "$VIRTUAL_KEYBOARD_THEME" ]; then
   		KBD_THEME_FILE='/usr/share/onboard/themes/'"$VIRTUAL_KEYBOARD_THEME"\'
+		echo "$KBD_THEME_FILE"
 		if [[ -f "$KBD_THEME_FILE" ]]; then
  			dbus-run-session -- dconf write /org/onboard/theme "$KBD_THEME_FILE" # set default theme
 		fi
 	fi
+ 	echo "$VIRTUAL_KEYBOARD_COLORS"
   	if [ -n "$VIRTUAL_KEYBOARD_COLORS" ]; then
   		KBD_COLOR_FILE='/usr/share/onboard/themes/'"$VIRTUAL_KEYBOARD_COLORS"\'
+		echo "$KBD_COLOR_FILE"
 		if [[ -f "$KBD_COLOR_FILE" ]]; then
  			dbus-run-session -- dconf write /org/onboard/theme-settings/color-scheme "$KBD_THEME_FILE" # set default theme
 		fi
