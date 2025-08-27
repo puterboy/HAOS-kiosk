8fb7fe7d4406b9dd7a3b6034f0d7755c74f605e2
GuntherSchulz <155998655+GuntherSchulz01@users.noreply.github.com>
2025-08-27 15:57:12 +0200

    Update run.sh fix typos

--- haoskiosk/run.sh
+++ haoskiosk/run.sh
@@ -146,26 +146,26 @@ if [ "$USE_VIRTUAL_KEYBOARD" = true ]; then
  	dbus-run-session -- dconf write /org/onboard/auto-show true # enable auto show
   	echo "$VIRTUAL_KEYBOARD_LAYOUT"
   	if [ -n "$VIRTUAL_KEYBOARD_LAYOUT" ]; then
-  		KBD_LAYOUT_FILE=\''/usr/share/onboard/layouts/'"$VIRTUAL_KEYBOARD_LAYOUT"\'
+  		KBD_LAYOUT_FILE='/usr/share/onboard/layouts/'"$VIRTUAL_KEYBOARD_LAYOUT"
 		echo "$KBD_LAYOUT_FILE" 
 		if [[ -f "$KBD_LAYOUT_FILE" ]]; then
-			dbus-run-session -- dconf write /org/onboard/layout "$KBD_LAYOUT_FILE" # set default layout
+			dbus-run-session -- dconf write /org/onboard/layout \'"$KBD_LAYOUT_FILE"\' # set default layout
 		fi
 	fi
     echo "$VIRTUAL_KEYBOARD_THEME"
   	if [ -n "$VIRTUAL_KEYBOARD_THEME" ]; then
-  		KBD_THEME_FILE=\''/usr/share/onboard/themes/'"$VIRTUAL_KEYBOARD_THEME"\'
+  		KBD_THEME_FILE='/usr/share/onboard/themes/'"$VIRTUAL_KEYBOARD_THEME"
 		echo "$KBD_THEME_FILE"
 		if [[ -f "$KBD_THEME_FILE" ]]; then
- 			dbus-run-session -- dconf write /org/onboard/theme "$KBD_THEME_FILE" # set default theme
+ 			dbus-run-session -- dconf write /org/onboard/theme \'"$KBD_THEME_FILE"\' # set default theme
 		fi
 	fi
  	echo "$VIRTUAL_KEYBOARD_COLORS"
   	if [ -n "$VIRTUAL_KEYBOARD_COLORS" ]; then
-  		KBD_COLOR_FILE=\''/usr/share/onboard/themes/'"$VIRTUAL_KEYBOARD_COLORS"\'
+  		KBD_COLOR_FILE='/usr/share/onboard/themes/'"$VIRTUAL_KEYBOARD_COLORS"
 		echo "$KBD_COLOR_FILE"
 		if [[ -f "$KBD_COLOR_FILE" ]]; then
- 			dbus-run-session -- dconf write /org/onboard/theme-settings/color-scheme "$KBD_THEME_FILE" # set default theme
+ 			dbus-run-session -- dconf write /org/onboard/theme-settings/color-scheme \'"$KBD_THEME_FILE"\' # set default colors
 		fi
 	fi
 	dbus-run-session -- dconf write /org/onboard/auto-show/enabled true # enable onboard keyboard
