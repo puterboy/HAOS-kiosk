2848a5d3f1fe7b4dc341226d0357007ebe8e0de8
GuntherSchulz <155998655+GuntherSchulz01@users.noreply.github.com>
2025-08-29 11:13:27 +0200

    Update run.sh cleanup

--- haoskiosk/run.sh
+++ haoskiosk/run.sh
@@ -394,9 +394,7 @@ if [ "$ONSCREEN_KEYBOARD" = true ]; then
 		dbus-run-session -- dconf write /org/onboard/auto-show/tablet-mode-detection-enabled false # shows keyboard only in tablet mode. I had to disable it to make it work
 		dbus-run-session -- dconf write /org/onboard/window/force-to-top true # always show in front
 	 	dbus-run-session -- gsettings set org.gnome.desktop.interface toolkit-accessibility true # disable gnome assessibility popup
-	  	#dbus-run-session -- gsettings set org.gnome.desktop.a11y.applications screen-keyboard-enabled true
-	    #dbus-run-session -- gsettings set org.gnome.desktop.a11y.applications screen-keyboard-enabled false
-		dbus-send --type=method_call --print-reply --dest=org.onboard.Onboard /org/onboard/Onboard/Keyboard org.onboard.Onboard.Keyboard.Hide
+	    dbus-run-session -- gsettings set org.gnome.desktop.a11y.applications screen-keyboard-enabled false # hide keyboard at startup
 	
 	fi
 
@@ -436,6 +434,7 @@ if [ "$ONSCREEN_KEYBOARD" = true ]; then
  		bashio::log.info "Backing up onscreen keyboard setup"
    
  		# Save only non-default settings
+   		rm -f $"KBD_PERSIST_FILE"
    		dconf dump / > $"KBD_PERSIST_FILE"
 	fi
 fi
