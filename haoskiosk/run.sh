5855aa8786d1a649362b12f5105459fa5ba6cb6b
GuntherSchulz <155998655+GuntherSchulz01@users.noreply.github.com>
2025-08-29 12:18:30 +0200

    Update run.sh cleanup

--- haoskiosk/run.sh
+++ haoskiosk/run.sh
@@ -387,15 +387,11 @@ if [ "$ONSCREEN_KEYBOARD" = true ]; then
 	    fi
 
 	  	### Enable keyboard to auto appear when inputting text
-	    ### dbus-run-session -- dconf write /org/onboard/auto-show true # enable auto show
-		#dbus-run-session -- dconf write /org/onboard/start-minimized true # hide keyboard at startup
 		dbus-run-session -- dconf write /org/onboard/xembed-onboard false # do not start in XEmbed mode 
 		dbus-run-session -- dconf write /org/onboard/auto-show/enabled true # enable auto show
 		dbus-run-session -- dconf write /org/onboard/auto-show/tablet-mode-detection-enabled false # shows keyboard only in tablet mode. I had to disable it to make it work
 		dbus-run-session -- dconf write /org/onboard/window/force-to-top true # always show in front
 	 	dbus-run-session -- gsettings set org.gnome.desktop.interface toolkit-accessibility true # disable gnome assessibility popup
-	    dbus-run-session -- gsettings set org.gnome.desktop.a11y.applications screen-keyboard-enabled false # hide keyboard at startup
-	
 	fi
 
 	### Launch keyboard
