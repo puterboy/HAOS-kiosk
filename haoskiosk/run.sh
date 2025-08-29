ff6bdcf66c72233a7cb9243f46b2538542017004
GuntherSchulz <155998655+GuntherSchulz01@users.noreply.github.com>
2025-08-30 00:03:38 +0200

    Update run.sh

--- haoskiosk/run.sh
+++ haoskiosk/run.sh
@@ -59,7 +59,7 @@ KBD_FILE_MUST_PERSIST=""
 KBD_PERSIST_FILE="/config/usr_custom_keyboad.ini"
 cleanup() {
     local exit_code=$?
-	[ -n "$KBD_FILE_MUST_PERSIST" ] && ! [ $(rm -f "$KBD_PERSIST_FILE) ] && dconf dump / > "$KBD_PERSIST_FILE"
+	#[ -n "$KBD_FILE_MUST_PERSIST" ] && ! [ $(rm -f "$KBD_PERSIST_FILE) ] && dconf dump / > "$KBD_PERSIST_FILE"
 	[ -n "$(jobs -p)" ] && kill "$(jobs -p)"
 	[ -n "$TTY0_DELETED" ] && mknod -m 620 /dev/tty0 c 4 0
     exit "$exit_code"
