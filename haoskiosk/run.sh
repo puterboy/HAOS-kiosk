aa8c22b9fe6d831d9e306d4e53ad07a945daa114
GuntherSchulz <155998655+GuntherSchulz01@users.noreply.github.com>
2025-08-30 10:34:32 +0200

    Update run.sh fix persist logic and trap on more things

--- haoskiosk/run.sh
+++ haoskiosk/run.sh
@@ -64,8 +64,8 @@ cleanup() {
 	[ -n "$TTY0_DELETED" ] && mknod -m 620 /dev/tty0 c 4 0
     exit "$exit_code"
 }
-trap cleanup INT TERM EXIT
-#trap cleanup HUP INT QUIT ABRT TERM EXIT
+trap cleanup HUP INT QUIT ABRT TERM EXIT
+#trap cleanup INT TERM EXIT
 
 
 ################################################################################
@@ -355,7 +355,11 @@ setxkbmap -query  | sed 's/^/  /' #Log layout
 if [ "$ONSCREEN_KEYBOARD" = true ]; then
 	bashio::log.info "Configuring onscreen keyboard"
 
- 	if [ "$PERSIST_ONSCREEN_KEYBOARD_CONFIG" = true ] && [ -f "$KBD_PERSIST_FILE" ]; then
+	if [ "$PERSIST_ONSCREEN_KEYBOARD_CONFIG" = false ]
+  		rm -f "$KBD_PERSIST_FILE"
+ 	fi
+
+ 	if [ -f "$KBD_PERSIST_FILE" ]; then
   		bashio::log.info "Restoring onscreen keyboard setup"
 
  		### Load all non-default settings from file and apply them
