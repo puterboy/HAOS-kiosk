1a87a2eb55135907342f9b47a4777f273d13c637
puterboy <github@kosowsky>
2026-01-23 16:32:20 -0500

    - Updated example ultrasonic-trigger.py

--- haoskiosk/examples/ultrasonic-trigger.py
+++ haoskiosk/examples/ultrasonic-trigger.py
@@ -1,19 +1,36 @@
+#!/usr/bin/env sh
+"exec" "sudo" "$(dirname $(readlink -f $0))/venv/bin/python3" "$0" "$@"
+#"exec" "$(dirname $0)/venv/bin/python3" "$0" "$@"
+#Above lines used to invoke venv relative to current directory
+#See: https://stackoverflow.com/questions/20095351/shebang-use-interpreter-relative-to-the-script-path
+
+#Below shebang line only works if call strict from the script directory
+#!$(dirname $0)/venv/bin/python3
+#Below shebang line only works if already activated virtual environment
+#!/usr/bin/env python3
+
 ################################################################################
 # Add-on: HAOS Kiosk Display (haoskiosk)
 # File: ultrasonic-trigger.py
-# Version: 1.1.1
+# Version: 1.1.0
 # Copyright Jeff Kosowsky
 # Date: September 2025
 #
 # Use a FTDI FT232H USB-GPIO board to monitor the output of an ultrasonic
 # HC-SR04 type distance sensor
-#   - Print out distance every second
+#   - Print out distance every LOOPTIME seconds
 #   - Turn on monitor if distance < NEAR_ON_DIST for COUNT_ON_THRESH seconds
 #   - Turn off monitor if distance > FAR_OFF_DIST for COUNT_OFF_THRESH seconds
 #
-# Also, optionally, don't measure distance and leave display in DEFAULT_DISPLAY_STATE
-# if the HA sensor HA_BINARY_SENSOR is set and evaluates to true.
+# When measuring distance:
+#   - Take GPIO_READINGS_TO_AVERAGE and average the valid ones
+#   - Mark as invalid measurement if more than half of the readings are errors
+#   - Restart if more than INVALID_COUNT_THRESHOLD invalid measurements in a row
+#
+# Also, optionally, if the HA sensor HA_BINARY_SENSOR is set and evaluates to true,
+# then don't measure distance and leave display in DEFAULT_DISPLAY_STATE
 # This can be used to make the auto on/off  depend on the state of a sensor in HA.
+#
 #
 # NOTES:
 #   - Requires adding the following Python libraries: pyftdi, requests
@@ -21,24 +38,41 @@
 #   - Should run as root (e.g., 'sudo')
 #
 ################################################################################
+# pylint: disable=line-too-long
+# pylint: disable=invalid-name
+# pylint: disable=too-many-instance-attributes
+# pylint: disable=broad-except
+# pylint: disable=too-many-arguments
+# pylint: disable=too-many-positional-arguments
+# pylint: disable=too-many-branches
+# pylint: disable=too-many-statements
+# pylint: disable=too-many-locals
+# pylint: disable=too-many-lines
+################################################################################
 
+import logging
+import os
 import sys
 import time
+from datetime import datetime, timedelta
 import requests
-from pyftdi.gpio import GpioController
+from pyftdi.gpio import GpioController  # type: ignore[import-untyped]
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
-from datetime import datetime
-import logging
+
+#Relaunch 'unbuffered' if not already unbuffered so that you can pipe output real-time if desired
+if os.environ.get('PYTHONUNBUFFERED') != '1':
+    os.environ['PYTHONUNBUFFERED'] = '1'
+    os.execvp(sys.executable, [sys.executable] + sys.argv)
 
 # Suppress urllib3 retry warnings
 logging.getLogger("urllib3").setLevel(logging.ERROR)
 
 logging.basicConfig(
     stream=sys.stdout,
-#    level=logging.DEBUG,
     level=logging.INFO,
-    format="[%(asctime)s] %(levelname)s: [%(filename)s] %(message)s",
+#    level=logging.DEBUG,
+    format='%(asctime)s [%(funcName)s] %(levelname)s: %(message)s',
     datefmt="%H:%M:%S"
 )
 
@@ -49,15 +83,19 @@ logging.basicConfig(
 TRIG_PIN = 0                  # AD0 - Output
 ECHO_PIN = 1                  # AD1 - Input
 GPIO_READINGS_TO_AVERAGE = 5  # Number of distance readings to average
-WAIT_TIMEOUT = 0.05           # Timeout for wait_for_pin (seconds)
+WAIT_TIMEOUT = 0.05           # Timeout for wait_for_pin (seconds) (this is conservative)
                               # Note HC-SR04 pulls pin low after 38ms (which with speed of sound 343m/s is equivalent to ~6.5m each way)
-
 # HA general variables
 HA_PORT = 8123
 HA_BEARER_TOKEN = None       # Needed if using HA_BINARY_SENSOR
-HA_BINARY_SENSOR=None        # Optional binary sensor to determine whether to measure distance and turn on/off display
 
-DEFAULT_DISPLAY_STATE=True   # Default display state if HA_BINARY_SENSOR is 'True' (False=off; True=on)
+HA_BINARY_SENSOR=None        # Optional binary sensor to determine whether to measure distance and turn on/off display
+HA_BINARY_SENSOR_FRIENDLY_NAME=None #Optional Friendly Name for binary sensor
+if HA_BINARY_SENSOR_FRIENDLY_NAME is None and HA_BINARY_SENSOR is not None:
+    #Get string after last '.', replace '_' with space, capitalize words
+    HA_BINARY_SENSOR_FRIENDLY_NAME = HA_BINARY_SENSOR.rsplit('.', 1)[-1].replace('_', ' ').title()
+
+DEFAULT_DISPLAY_STATE=True  # Default display state if HA_BINARY_SENSOR is 'True' (False=off; True=on)
 
 # Configure REST API
 REST_PORT = 8080
@@ -67,8 +105,12 @@ REST_BEARER_TOKEN=""
 LOOP_TIME = 1                 # Target loop time (seconds) - i.e., target time between distance measurements
 NEAR_ON_DIST = 150            # Near distance threshold (in cm) before turning display on
 FAR_OFF_DIST = 200            # Far distance threshold (in cm) before turning display off
-COUNT_ON_THRESH = 3           # Number of 'near' distance measurements before turning on
-COUNT_OFF_THRESH = 5          # Number of 'far' distance measurements before turning off
+COUNT_ON_THRESH = 2           # Number of 'near' distance measurements before turning on
+COUNT_OFF_THRESH = 4          # Number of 'far' distance measurements before turning off
+
+
+INVALID_COUNT_THRESHOLD= 10   # Number of consecutive invalid measurements before restarting
+HTTP_TIMEOUT = 3              # Timeout for HTTP get and posts
 
 ################################################################################
 ### Ultrasonic distance sensing
@@ -81,34 +123,39 @@ gpio = GpioController()
 try:
     gpio.configure('ftdi://ftdi:232h/1', direction=TRIG_MASK)  # TRIG = output, ECHO = input
 except Exception as e:
-    logging.error(f"[ultrasonic_trigger] Error: GPIO init failed")
-    print("Exiting due to GPIO initialization failure")
+    logging.error("Error: Ultrasonic trigger GPIO init failed")
+    print("Exiting due to Ultrasonic GPIO initialization failure")
     sys.exit(1)
 
-def send_trigger_pulse():
+def send_trigger_pulse()-> bool:
+    """Send ultrasonic trigger pulse"""
     try:
         gpio.write(0)
         time.sleep(0.000002)  # 2 µs
         gpio.write(TRIG_MASK)  # Set TRIG high
-        time.sleep(0.00001)    # 10 µs pulse
+        time.sleep(0.00001)    # 10 us pulse
         gpio.write(0)
         return True
     except Exception as e:
-        logging.debug(f"[send_trigger_pulse] Error: GPIO write failed")
+        logging.debug("GPIO write failed (%s)", e)
         return False
 
-def wait_for_pin(mask, level, timeout=WAIT_TIMEOUT):
+def wait_for_pin(echo_mask: int, echo_level: bool, timeout: float=WAIT_TIMEOUT) -> int | None:
+    """Wait for pin"""
+    timeout_ns = int(timeout * 1e9)
+    start_ns = time.monotonic_ns()
     try:
-        start = time.monotonic()
-        while time.monotonic() - start < timeout:
-            if bool(gpio.read() & mask) == level:
-                return time.monotonic_ns()
+        while (time_ns := time.monotonic_ns()) - start_ns < timeout_ns:
+            if bool(gpio.read() & echo_mask) == echo_level:
+                return time_ns
         return None
     except Exception as e:
-        logging.debug(f"[wait_for_pin] Error: GPIO read failed")
+        logging.debug("Error: GPIO read failed (%s)", e)
         return None
 
-def measure_distance():
+invalid_count = 0  # Number of consecutive invalid measurements
+def measure_distance() -> float | None:
+    """Measure distance"""
     distances = []
     errors = 0
     for _ in range(GPIO_READINGS_TO_AVERAGE):
@@ -118,15 +165,13 @@ def measure_distance():
 
         start_time = wait_for_pin(ECHO_MASK, True)
         if start_time is None:
-            if logging.getLogger().isEnabledFor(logging.DEBUG):
-                print("Timeout waiting for ECHO to go HIGH")
+            logging.debug("Timeout waiting for ECHO to go HIGH")
             errors += 1
             continue
 
         end_time = wait_for_pin(ECHO_MASK, False)
         if end_time is None:
-            if logging.getLogger().isEnabledFor(logging.DEBUG):
-                print("Timeout waiting for ECHO to go LOW")
+            logging.debug("Timeout waiting for ECHO to go LOW")
             errors += 1
             continue
 
@@ -138,8 +183,18 @@ def measure_distance():
             errors += 1
         time.sleep(0.01)  # Small delay between readings to avoid sensor overload
 
+    global invalid_count
     if errors >= (GPIO_READINGS_TO_AVERAGE / 2):
+        invalid_count +=1
+        if invalid_count > INVALID_COUNT_THRESHOLD:
+            logging.error("Error: Too many invalid measurements (%d), restarting...", INVALID_COUNT_THRESHOLD)
+            try:
+                gpio.close()
+            except Exception:
+                pass
+            os.execv(sys.executable, [sys.executable] + sys.argv)  # Restart...
         return None
+    invalid_count = 0  # Reset invalid counter
     return sum(distances) / len(distances) if distances else None
 
 ################################################################################
@@ -151,59 +206,44 @@ retries = Retry(total=3, backoff_factor=0.1, status_forcelist=[429, 500, 502, 50
 session.mount("http://", HTTPAdapter(max_retries=retries))
 
 def display_state() -> bool:
+    """Return display state"""
     url = f"http://localhost:{REST_PORT}/is_display_on"
     try:
         response = session.get(
             url,
-            headers={"Authorization": f"Bearer {REST_BEARER_TOKEN}"}
+            headers={"Authorization": f"Bearer {REST_BEARER_TOKEN}"},
+            timeout = HTTP_TIMEOUT,
         )
         response.raise_for_status()
         data = response.json()
         if not data.get("success", False):
-            logging.debug(f"[display_state] Error: Failed to get state")
+            logging.error("Error: Failed to get display state")
             return False
         return data["display_on"]
     except (requests.RequestException, ValueError):
-        logging.debug(f"[display_state] Error: Request failed")
+        logging.debug("Error: Request failed")
         return False
 
-def display_state2() -> bool: #Alternative - uses and parses 'xset -q' command
-    url = f"http://localhost:{REST_PORT}/xset"
-    try:
-        response = session.post(
-            url,
-            headers={"Authorization": f"Bearer {REST_BEARER_TOKEN}"},
-            json={"args": "-q"}
-        )
-        response.raise_for_status()
-        data = response.json()
-        if not data.get("success", False) or not data.get("result", {}).get("success", False):
-            logging.debug(f"[display_state2] Error: Failed to get state")
-            return False
-        stdout_text = data["result"].get("stdout", "")
-        return "Monitor is On" in stdout_text
-    except (requests.RequestException, ValueError):
-        logging.debug(f"[display_state2] Error: Request failed")
-        return False
-
-def display_state_print():
+def display_state_print() -> None:
+    """Print display state"""
     global display
     try:
-        new_display = display_state()
-        if new_display is True:
-            display = True
-            print('Display is ON')
-        elif new_display is False:
-            print('Display is OFF')
+        display = display_state()
+        if display is True:
+            print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M')}] Display is ON")
+        else:
+            print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M')}] Display is OFF")
     except (requests.RequestException, ValueError):
         print('Display is INVALID')
 
 def display_on() -> bool:
+    """Turn display on"""
     url = f"http://localhost:{REST_PORT}/display_on"
     try:
         response = session.post(
             url,
-            headers={"Authorization": f"Bearer {REST_BEARER_TOKEN}"}
+            headers={"Authorization": f"Bearer {REST_BEARER_TOKEN}"},
+            timeout = HTTP_TIMEOUT,
         )
         response.raise_for_status()
         data = response.json()
@@ -213,20 +253,14 @@ def display_on() -> bool:
     except (requests.RequestException, ValueError):
         return False
 
-def display_on_print():
-    if display_on():
-        print("***Turning display ON***")
-        global display
-        display = True
-    else:
-        print("FAILED to turn display ON")
-
 def display_off() -> bool:
+    """Turn display off"""
     url = f"http://localhost:{REST_PORT}/display_off"
     try:
         response = session.post(
             url,
-            headers={"Authorization": f"Bearer {REST_BEARER_TOKEN}"}
+            headers={"Authorization": f"Bearer {REST_BEARER_TOKEN}"},
+            timeout = HTTP_TIMEOUT,
         )
         response.raise_for_status()
         data = response.json()
@@ -236,40 +270,66 @@ def display_off() -> bool:
     except (requests.RequestException, ValueError):
         return False
 
-def display_off_print():
+last_display_time = datetime.now()
+def display_on_print() -> None:
+    """Turn on display and show duration since last on"""
+    global last_display_time
+    old_display_time = last_display_time
+    last_display_time = datetime.now()
+    display_time_diff = last_display_time - old_display_time
+    display_time_diff = display_time_diff - timedelta(microseconds=display_time_diff.microseconds)
+
+    if display_on():
+        print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M')}] ***Turning display ON*** (Duration: {display_time_diff})")
+        global display
+        display = True
+    else:
+        logging.error("Error: FAILED to turn display ON")
+
+def display_off_print() ->None:
+    """Turn off display and show duration since last off"""
+    global last_display_time
+    old_display_time = last_display_time
+    last_display_time = datetime.now()
+    display_time_diff = last_display_time - old_display_time
+    display_time_diff = display_time_diff - timedelta(microseconds=display_time_diff.microseconds)
+
     if display_off():
-        print("***Turning display OFF***")
+        print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M')}] ***Turning display OFF*** (Duration: {display_time_diff})")
         global display
         display = False
     else:
-        print("FAILED to turn display OFF")
+        logging.error("Error: FAILED to turn display OFF")
 
-def is_binary_sensor() -> bool:
-    if HA_BINARY_SENSOR is None: return None
-    url = f"http://localhost:{HA_PORT}/api/states/{HA_BINARY_SENSOR}"
+def ha_binary_sensor_state(sensor: str) -> bool | None:
+    """Show state of binary sensor used to turn/off ultrasonic-governed display mechanism"""
+    if sensor is None:
+        return None
+    url = f"http://localhost:{HA_PORT}/api/states/{sensor}"
     try:
         response = session.get(
             url,
-            headers={"Authorization": f"Bearer {HA_BEARER_TOKEN}"}
+            headers={"Authorization": f"Bearer {HA_BEARER_TOKEN}"},
+            timeout = HTTP_TIMEOUT,
         )
         response.raise_for_status()
         data = response.json()
 
         state = data.get("state")
         if state not in ("on", "off"):
-            logging.debug(f"[is_binary_sensor] Unexpected state value: {state}")
-            return False
+            logging.debug("Unexpected state value: %s", state)
+            return None
         return state == "on"
 
     except (requests.RequestException, ValueError):
-        logging.debug(f"[is_binary_sensor] Error: Request failed")
-        return False
+        logging.error("Error: Request failed")
+        return None
 
 ################################################################################
 ### Main loop
 
 display = False
-loop_num = -1;
+loop_num = -1
 count = 0
 binary_sensor_state = None
 try:
@@ -279,14 +339,14 @@ try:
         if not loop_num % 60:  # HA_BINARY_SENSOR state once a minute
                                # Also, update display state in case gets out of sync
             old_binary_sensor_state = binary_sensor_state
-            binary_sensor_state = is_binary_sensor();
-            if binary_sensor_state is not None and binary_sensor_state != old_binary_sensor_state:
-                print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M')}] {HA_BINARY_SENSOR}={binary_sensor_state}")
-                if binary_sensor_state:
+            binary_sensor_state = ha_binary_sensor_state(HA_BINARY_SENSOR)
+            if binary_sensor_state is not None and binary_sensor_state != old_binary_sensor_state:  # Status of binary_sensor_state changed
+                print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M')}] '{HA_BINARY_SENSOR_FRIENDLY_NAME}' = {binary_sensor_state}")
+                if binary_sensor_state:  # Binary sensor turned on so set to default state
                     if DEFAULT_DISPLAY_STATE:
-                        display_on_print()  # Turn on display
+                        display_on_print()  # Default true
                     else:
-                        display_off_print()  # Turn off display
+                        display_off_print()  # Default false
             if not binary_sensor_state:
                 display_state_print()  # Set and show display state every 60 seconds
 
@@ -299,14 +359,12 @@ try:
             distance_ft = distance / 30.48
             print(f"Distance: {distance_ft:.2f} ft ({int(distance)} cm)")
             if distance < NEAR_ON_DIST:
-                if count < 0:
-                    count=0
+                count = max(count, 0)
                 count += 1
                 if display is False and count >= COUNT_ON_THRESH:
                     display_on_print()  # Turn ON display
             elif distance > FAR_OFF_DIST:
-                if count > 0:
-                    count=0
+                count = min(count, 0)
                 count -= 1
                 if display is True and count <= -COUNT_OFF_THRESH:
                     display_off_print()  # Turn OFF display
@@ -315,8 +373,7 @@ try:
 
         loop_duration = time.monotonic() - loop_start
         sleep_time = max(0, LOOP_TIME - loop_duration)
-        if logging.getLogger().isEnabledFor(logging.DEBUG):
-            logging.debug(f"Sleeping for {sleep_time:.3f} seconds")
+        logging.debug("Sleeping for %.3f seconds", sleep_time)
         if sleep_time > 0:
             time.sleep(sleep_time)
 
@@ -324,7 +381,7 @@ except KeyboardInterrupt:
     try:
         gpio.close()
     except Exception as e:
-        logging.error(f"[main] Error: GPIO close failed")
+        logging.error("Error: GPIO close failed")
     print("\nExiting.")
 
 # vim: set filetype=python :
